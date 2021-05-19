ef394fe07cf26e81628cdc53ecf0bcc4d0a418ed
Alisson Youssf <youssfbr@gmail.com>
2021-05-19 18:06:57 -0300

    ClienteController test

---
+++ backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
@@ -0,0 +1,15 @@
+package com.github.youssfbr.logistica.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+	@GetMapping
+	public String listar() {
+		return "Teste";
+	}
+}
