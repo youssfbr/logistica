37217f9c9be14949aa2a8b8e35559ffe7703520b
Alisson Youssf <youssfbr@gmail.com>
2021-05-20 14:31:00 -0300

    Cliente Delete

--- backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
+++ backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,18 @@ public class ClienteController {
 				
 		return ResponseEntity.ok(entity);
 	}	
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		
+		if (!repository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}		
+			
+		repository.deleteById(id);		
+				
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 }
