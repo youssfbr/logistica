338b15cdc43703d52cc2a5b976dc96912dc67e43
Alisson Youssf <youssfbr@gmail.com>
2021-05-20 11:08:18 -0300

    Cliente findById

--- backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
+++ backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
@@ -2,7 +2,9 @@ package com.github.youssfbr.logistica.api.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,15 @@ public class ClienteController {
 	private ClienteRepository repository;
 	
 	@GetMapping
-	public List<Cliente> findAll() {
-		return repository.findAll();
+	public ResponseEntity<List<Cliente>> findAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}	
+	
+	@GetMapping("{id}")
+	public ResponseEntity<Cliente> findByID(@PathVariable Long id) {
+		return repository.findById(id)				
+				.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
 	}
 	
 }
--- backend/src/main/java/com/github/youssfbr/logistica/domain/repository/ClienteRepository.java
+++ backend/src/main/java/com/github/youssfbr/logistica/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.github.youssfbr.logistica.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.github.youssfbr.logistica.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 
+	List<Cliente> findByNomeContaining(String nome);
 }
