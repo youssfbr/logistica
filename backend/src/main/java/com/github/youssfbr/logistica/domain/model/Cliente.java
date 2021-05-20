1f2afc73a3e8a1ecdc93b880d6a67da1eb9b0ece
Alisson Youssf <youssfbr@gmail.com>
2021-05-19 22:11:03 -0300

    Cliente entity

--- backend/src/main/java/com/github/youssfbr/logistica/domain/model/Cliente.java
+++ backend/src/main/java/com/github/youssfbr/logistica/domain/model/Cliente.java
@@ -1,12 +1,25 @@
 package com.github.youssfbr.logistica.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_cliente")
 public class Cliente {
 
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
