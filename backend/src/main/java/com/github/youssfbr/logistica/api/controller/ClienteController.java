103177053ae092bc4bdec0a2d3c47314f4ef9c17
Alisson Youssf <youssfbr@gmail.com>
2021-05-19 22:24:36 -0300

    Cliente findAll test

--- backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
+++ backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package com.github.youssfbr.logistica.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,22 +14,14 @@ import com.github.youssfbr.logistica.domain.model.Cliente;
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+	
+	@PersistenceContext
+	private EntityManager manager;	
 
 	@GetMapping
-	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Alisson");
-		cliente1.setTelefone("85 99999-11111");
-		cliente1.setEmail("youssfbr@gmail.com");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Link da Silva");
-		cliente2.setTelefone("85 99999-34567");
-		cliente2.setEmail("link@gmail.com");
-		
-		return Arrays.asList(cliente1, cliente2);
+	public List<Cliente> findAll() {
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 	
 }
