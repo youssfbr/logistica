a79076dd0bd095500c258e5a94915db556d2858b
Alisson Youssf <youssfbr@gmail.com>
2021-05-20 11:27:02 -0300

    Cliente POST

--- backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
+++ backend/src/main/java/com/github/youssfbr/logistica/api/controller/ClienteController.java
@@ -1,12 +1,16 @@
 package com.github.youssfbr.logistica.api.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.github.youssfbr.logistica.domain.model.Cliente;
 import com.github.youssfbr.logistica.domain.repository.ClienteRepository;
@@ -30,6 +34,16 @@ public class ClienteController {
 		return repository.findById(id)				
 				.map(ResponseEntity::ok)
 				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Cliente> insert(@RequestBody Cliente entity) {
+		entity = repository.save(entity);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(entity.getId()).toUri();
+				
+		return ResponseEntity.created(uri).body(entity);
 	}
 	
 }
